4be75e907af408b05f9d15e09a129a578972dbf7
EEEVONNEEE <125324589+NGYeung@users.noreply.github.com>
2024-08-16 17:39:07 -0400

    Update CLIP_Datasets.py

--- Datasets/CLIP_Datasets.py
+++ Datasets/CLIP_Datasets.py
@@ -52,7 +52,7 @@ class Meme_DataSet():
 
     def __getitem__(self, idx):
         
-        maxlength = 80 # REMEMBER TO ADJUST THIS!!!!!!!!
+        maxlength = 60
         text = pd.read_csv(self.text_path)
         label = pd.read_csv(self.labelpath)
         self.label = list(label['sarcasm'])[idx]
@@ -61,123 +61,6 @@ class Meme_DataSet():
         doc = self.nlp(self.text)
         lemmatized_words = [token.lemma_ for token in doc if not token.is_stop]
         self.text = ' '.join(lemmatized_words)
-        ct = len(os.listdir(self.img_dir)[idx])
-        img_path = os.path.join(self.img_dir, os.listdir(self.img_dir)[idx])
-     
-        
-        transform = transforms.Compose([
-                               transforms.ToPILImage(),
-                               transforms.Resize((224, 224)),
-                               transforms.ToTensor()
-                                ])
-        
-        
-        transform = transforms.Compose([
-            transforms.Resize((224, 224)),  
-            transforms.ToTensor(),            
-        ])
-        
-        
-        with open(img_path, 'rb') as file:
-            corrupted_data = file.read()
-        
-        # clean corrupted images. strip the last byte
-        img = Image.open(io.BytesIO(corrupted_data))
-        
-        
-        img = img.convert("RGB")
-        
-        buffer = io.BytesIO()
-        img.save(buffer, format="JPEG")
-        cleaned_data = buffer.getvalue()
-        
-        im = Image.open(io.BytesIO(cleaned_data))
-        image_batch = transform(im)
-        
-           
-   
-        if self.img_preprocess:
-            image = self.img_preprocess(images = image_batch, return_tensors = 'pt')
- 
-            
-        
-        
-        #CAPTION at entry 1
-
-        text_batch = self.text_tokenizer.encode_plus(
-            self.text,
-            add_special_tokens=True,  # Add [CLS] and [SEP]
-            max_length=maxlength,
-            padding='longest',
-            truncation=True,  # Truncate to max_length
-            return_attention_mask=True,  # Return attention masks
-            return_tensors='pt'  # Return PyTorch tensors
-            
-            )
-      
-        
-        return {'image': image['pixel_values'], 'input_ids': text_batch['input_ids'], 'attention': text_batch['attention_mask'], 'label': self.label}
-    
-    def getimages(self, idx):
-        '''
-        Used to return unprocessed image
-
-        '''
-        for i in idx:
-            item = os.listdir(self.img_dir)[i]
-            img_path = os.path.join(self.img_dir,item)
-            image = cv2.imread(img_path)
-            #image = cv2.resize(image,(224, 224,3))
-            self.images.append(image)
-            
-        return self.images
-    
-
-class Meme_Classify():
-    
-        
-    def __init__(self, img_dir, text_file ='Datasets/TEXT_sentences.csv', label = 'Datasets/Sentiments.csv' ,img_model = 'DINOv2', text_model = 'DistilledBert'):
-    
-        self.nlp = spacy.load("en_core_web_sm")
-
-        self.text_path = text_file
-        self.labelpath = label
-        self.img_dir = img_dir
-        self.text_tokenizer =None
-        self.img_preprocess = None
-        if img_model == 'DINOv2':
-            self.img_preprocess = AutoImageProcessor.from_pretrained('facebook/dinov2-base')
-        
-        # Fill in the efficient net
-        if text_model == 'DistilledBert':
-            self.text_tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-uncased')
-        self.images = []
-        self.text = []
-        self.label = []
-
-    def __len__(self):
-        item_list = os.listdir(self.img_dir)
-        return len(item_list)
-
-    def __getitem__(self, idx):
-        
-        maxlength = 80 # REMEMBER TO ADJUST THIS!!!!!!!!
-        text = pd.read_csv(self.text_path)
-        label = pd.read_csv(self.labelpath)
-        if list(label['sarcasm'])[idx] == "1":
-            self.label = '[SEP] a sarcastic meme'
-        else:
-            self.label = '[SEP] a regular meme'
-            
-        #Lemmatization and filter stop words
-       
-        self.text = list(text['text_corrected'])[idx]
-        doc = self.nlp(self.text)
-        lemmatized_words = [token.lemma_ for token in doc if not token.is_stop]
-        self.text = ' '.join(lemmatized_words)
-        
-        self.text = self.text + self.label
-    
         ct = len(os.listdir(self.img_dir)[idx])
         img_path = os.path.join(self.img_dir, os.listdir(self.img_dir)[idx])
      
@@ -248,6 +131,125 @@ class Meme_Classify():
             self.images.append(image)
             
         return self.images
+    
+
+class Meme_Classify():
+    
+        
+    def __init__(self, img_dir, text_file ='Datasets/TEXT_sentences.csv', label = 'Datasets/Sentiments.csv' ,img_model = 'DINOv2', text_model = 'DistilledBert'):
+    
+        self.nlp = spacy.load("en_core_web_sm")
+
+        self.text_path = text_file
+        self.labelpath = label
+        self.img_dir = img_dir
+        self.text_tokenizer =None
+        self.img_preprocess = None
+        if img_model == 'DINOv2':
+            self.img_preprocess = AutoImageProcessor.from_pretrained('facebook/dinov2-base')
+        
+        # Fill in the efficient net
+        if text_model == 'DistilledBert':
+            self.text_tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-uncased')
+        self.images = []
+        self.text = []
+        self.label = []
+
+    def __len__(self):
+        item_list = os.listdir(self.img_dir)
+        return len(item_list)
+
+    def __getitem__(self, idx):
+        
+        maxlength = 128 # REMEMBER TO ADJUST THIS!!!!!!!!
+        text = pd.read_csv(self.text_path)
+        label = pd.read_csv(self.labelpath)
+        
+        if list(label['sarcasm'])[idx] == 1:
+            self.label = 'a sarcastic meme'
+        else:
+            self.label = 'a regular meme'
+            
+        #Lemmatization and filter stop words
+       
+        self.text = list(text['text_corrected'])[idx]
+        doc = self.nlp(self.text)
+        lemmatized_words = [token.lemma_ for token in doc if not token.is_stop]
+        self.text = ' '.join(lemmatized_words)
+        
+        #self.text = self.text + self.label
+    
+        ct = len(os.listdir(self.img_dir)[idx])
+        img_path = os.path.join(self.img_dir, os.listdir(self.img_dir)[idx])
+     
+        
+        transform = transforms.Compose([
+                               transforms.ToPILImage(),
+                               transforms.Resize((224, 224)),
+                               transforms.ToTensor()
+                                ])
+        
+        
+        transform = transforms.Compose([
+            transforms.Resize((224, 224)),  
+            transforms.ToTensor(),            
+        ])
+        
+        
+        with open(img_path, 'rb') as file:
+            corrupted_data = file.read()
+        
+        # clean corrupted images. strip the last byte
+        img = Image.open(io.BytesIO(corrupted_data))
+        
+        
+        img = img.convert("RGB")
+        
+        buffer = io.BytesIO()
+        img.save(buffer, format="JPEG")
+        cleaned_data = buffer.getvalue()
+        
+        im = Image.open(io.BytesIO(cleaned_data))
+        image_batch = transform(im)
+        
+           
+   
+        if self.img_preprocess:
+            image = self.img_preprocess(images = image_batch, return_tensors = 'pt')
+ 
+            
+        
+        
+        #CAPTION at entry 1
+        sentence_pair = (self.text, self.label)
+   
+        text_batch = self.text_tokenizer.encode_plus(
+            sentence_pair,
+            add_special_tokens=True,  # Add [CLS] and [SEP]
+            max_length=maxlength,
+            padding='max_length',
+            truncation=True,  # Truncate to max_length
+            return_attention_mask=True,  # Return attention masks
+            return_tensors='pt'  # Return PyTorch tensors
+            
+            )
+      
+        
+        return {'image': image['pixel_values'], 'input_ids': text_batch['input_ids'], 'attention': text_batch['attention_mask'], 'label': self.label}
+    
+    def getimages(self, idx):
+        '''
+        Used to return unprocessed image
+
+        '''
+        for i in idx:
+            item = os.listdir(self.img_dir)[i]
+            img_path = os.path.join(self.img_dir,item)
+            image = cv2.imread(img_path)
+            #image = cv2.resize(image,(224, 224,3))
+            self.images.append(image)
+            
+        return self.images
 
     
 class Meme_Verify():
@@ -281,7 +283,7 @@ class Meme_Verify():
         maxlength = 80 # REMEMBER TO ADJUST THIS!!!!!!!!
     
         label = pd.read_csv(self.labelpath)
-        if list(label['sarcasm'])[idx] == "1":
+        if list(label['sarcasm'])[idx] == 1:
             self.label = 'a sarcastic meme'
         else:
             self.label = 'a regular meme'
